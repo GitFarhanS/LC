023069e7f2c9b0b30bf5f40ebdc45f36eff9970b
Farhan Shaikh <34765527+GitFarhanS@users.noreply.github.com>
2025-10-08 20:32:23 +0100

    Time: 1 ms (57.71%), Space: 19.1 MB (10.41%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i in range(len(nums)):
+            remainder = target - nums[i]
+
+            if remainder in seen:
+                return [i, seen[remainder]]
+            else:
+                seen[nums[i]] = i
+            
